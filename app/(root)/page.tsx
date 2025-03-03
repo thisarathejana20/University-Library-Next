bc974255cecbe286f8d25fa8761f8007ecb1c9b9
thisaramax20 <thisarathejana20@gmail.com>
2025-03-03 12:55:06 +0530

    add temporarily constants to focus on UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,17 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import React from "react";
+import { sampleBooks } from "@/constants";
 
 const page = () => {
   return (
     <>
-      <BookOverview />
-      <BookList />
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 };
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-const BookOverview = () => {
+const BookOverview = ({
+  title,
+  author,
+  genre,
+  rating,
+  total_copies,
+  description,
+  color,
+  cover,
+}) => {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
-        <h1>A Long Book Title</h1>
+        <h1>{title}</h1>
       </div>
     </section>
   );
