1d3cd3b8ec69bddec1a1cd94f37b98b3ae80559e
thisaramax20 <thisarathejana20@gmail.com>
2025-03-05 19:01:16 +0530

    add admin layout

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_ENDPOINT!,
     imagekit: {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
